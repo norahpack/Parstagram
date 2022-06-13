c2c8f1dfcbbf7663a78418262b4af2f68f22ab89
norahpack <norahpack14@gmail.com>
2022-06-13 12:33:58 -0700

    Splash screen

---
+++ app/src/main/java/com/example/parstagram/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.parstagram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+
+    }
+}
