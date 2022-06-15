f35b406ada2145a424faa6759d132abc35feb27e
norahpack <norahpack14@gmail.com>
2022-06-14 17:42:12 -0700

    likes

--- app/src/main/java/com/example/parstagram/PostDetails.java
+++ app/src/main/java/com/example/parstagram/PostDetails.java
@@ -1,10 +1,14 @@
 package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -27,7 +31,10 @@ public class PostDetails extends AppCompatActivity {
     TextView tvDescription;
     ImageView ivImage;
     TextView tvLikes;
+    Button btnLikes;
     int likes;
+    boolean liked = false;
+    Drawable likeBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +46,37 @@ public class PostDetails extends AppCompatActivity {
         tvDescription=findViewById(R.id.tvDescription);
         ivImage=findViewById(R.id.ivImage);
         tvLikes=findViewById(R.id.tvLikes);
+        btnLikes=findViewById(R.id.btnLikes);
+
 
         Post post = (Post) Parcels.unwrap(getIntent().getParcelableExtra(Post.class.getSimpleName()));
         this.bind(post);
+
+        btnLikes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Drawable newBackground;
+                    int change_likes;
+
+                    if (liked){
+                        newBackground = AppCompatResources.getDrawable(PostDetails.this, R.drawable.ufi_heart);
+                        liked = false;
+                        change_likes=-1;
+                    } else {
+                        newBackground = AppCompatResources.getDrawable(PostDetails.this, R.drawable.ufi_heart_active);
+                        liked = true;
+                        change_likes=1;
+                    }
+                    post.setLikes(post.getLikes()+change_likes);
+                    tvLikes.setText(String.valueOf(post.getLikes()));
+                try {
+                    post.save();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                btnLikes.setBackground(newBackground);
+            }
+        });
 
 
     }
@@ -60,5 +95,16 @@ public class PostDetails extends AppCompatActivity {
         if (image != null){
             Glide.with(this).load(image.getUrl()).into(ivImage);
         }
+
+        if(liked) {
+            likeBackground = AppCompatResources.getDrawable(PostDetails.this, R.drawable.ufi_heart_active);
+        } else {
+            likeBackground = AppCompatResources.getDrawable(PostDetails.this, R.drawable.ufi_heart);
+
+        }
+        btnLikes.setBackground(likeBackground);
     }
+
+
+
 }
