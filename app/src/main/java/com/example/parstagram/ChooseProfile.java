e5edd24f99d8e6b3710a14faa496d4b6cfa41f47
norahpack <norahpack14@gmail.com>
2022-06-15 18:06:29 -0700

    can get to choose profile image screen 3

--- app/src/main/java/com/example/parstagram/ChooseProfile.java
+++ app/src/main/java/com/example/parstagram/ChooseProfile.java
@@ -1,13 +1,27 @@
 package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 public class ChooseProfile extends AppCompatActivity {
 
@@ -26,9 +40,24 @@ public class ChooseProfile extends AppCompatActivity {
             public void onClick(View v) {
                 int idChecked = choiceList.getCheckedRadioButtonId();
                 finalChoice=findViewById(idChecked);
-                finalChoice.getButtonDrawable();
-                Intent i = new Intent(ChooseProfile.this, FeedActivity.class);
-                startActivity(i);
+                //Drawable choiceImage=finalChoice.getButtonDrawable();
+                //Drawable choiceImage= AppCompatResources.getDrawable(ChooseProfile.this, R.drawable.ufi_heart_active);
+                String choiceImage = "ufi_heart_active";
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                currentUser.put("profilePic", choiceImage);
+
+                currentUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null){
+                        }
+                        Intent i = new Intent(ChooseProfile.this, FeedActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                });
+
             }
         });
     }
