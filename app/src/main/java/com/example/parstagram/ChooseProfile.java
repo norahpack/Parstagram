6450324fed3c3068dbdebcd4e4acac2b987e6296
norahpack <norahpack14@gmail.com>
2022-06-15 17:27:19 -0700

    can get to choose profile image screen 2

--- app/src/main/java/com/example/parstagram/ChooseProfile.java
+++ app/src/main/java/com/example/parstagram/ChooseProfile.java
@@ -2,18 +2,34 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class ChooseProfile extends AppCompatActivity {
 
     RadioGroup choiceList;
+    Button btnUpdate;
+    RadioButton finalChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_profile);
         choiceList=findViewById(R.id.choiceList);
-        int idChecked = choiceList.getCheckedRadioButtonId();
+        btnUpdate=findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int idChecked = choiceList.getCheckedRadioButtonId();
+                finalChoice=findViewById(idChecked);
+                finalChoice.getButtonDrawable();
+                Intent i = new Intent(ChooseProfile.this, FeedActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
